b14a2bf75212ee5009b2107db7acc998576e465e
aidanhorton <aidan.horton@virginmedia.com>
2017-02-10 15:11:12 +0000

    Fixed modules, and added spellcheck
    Spellcheck module is different to auto-complete. Auto-complete will guess the word based on empty letters and matching exactly. Spell-Check will take a word spelled incorrectly and return the word it is closest to. (Possibly make it the other way around?)

--- pyfi/AutoComplete.py
+++ pyfi/AutoComplete.py
@@ -12,8 +12,8 @@ class AutoComplete(): #Author Aidan Horton
                 if match: return currWord
                         
     def AutoCompleteFull(self, word):
-        engDict, i, bestWords, word = open("Dictionary.txt", "r"), 0, [], word.lower()
-        while i != "*":
+        engDict, currWord, bestWords, word = open("Dictionary.txt", "r"), "", [], word.lower()
+        while currWord != "*":
             currWord, match = engDict.readline().rstrip(), True
             if len(currWord) == len(word):
                 for i in range(len(word)):
@@ -22,3 +22,14 @@ class AutoComplete(): #Author Aidan Horton
                         break
                 if match: bestWords.append(currWord)
         return bestWords
+
+    def SpellCheck(self, word):
+        engDict, currWord, bestScore, word = open("Dictionary.txt", "r"), "", ["", 0], word.lower()
+        while currWord != "*":
+            currWord, match = engDict.readline().rstrip(), True
+            if len(currWord) == len(word):
+                score = 0
+                for i in range(len(word)):
+                    if word[i] == currWord[i]: score += 1
+                if score > bestScore[1]: bestScore = [currWord, score]
+        return bestScore[0]
