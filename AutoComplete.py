2e61a6fbb835adaad2c666440632d3d2e8d38db5
aidanhorton <aidan.horton@virginmedia.com>
2017-02-24 12:43:09 +0000

    Fixed me accidentally undoing stuff

---
+++ AutoComplete.py
@@ -0,0 +1,39 @@
+class AutoComplete(): #Author Aidan Horton
+    
+    def AutoCompleteFast(word):
+        engDict, i, word = open("Dictionary.txt", "r"), 0, word.lower()
+        while currWord != "*":
+            currWord, match = engDict.readline().rstrip(), True
+            if len(currWord) == len(word):
+                for i in range(len(word)):
+                    if word[i].isalpha() and word[i] != currWord[i]:
+                        match = False
+                        break
+                if match: return currWord
+                        
+    def AutoCompleteFull(word):
+        engDict, currWord, bestWords, word = open("Dictionary.txt", "r"), "", [], word.lower()
+        while currWord != "*":
+            currWord, match = engDict.readline().rstrip(), True
+            if len(currWord) == len(word):
+                for i in range(len(word)):
+                    if word[i].isalpha() and word[i] != currWord[i]:
+                        match = False
+                        break
+                if match: bestWords.append(currWord)
+        return bestWords
+
+    def SpellCheck(word):
+        engDict, currWord, bestScore, word = open("Dictionary.txt", "r"), "", ["", 0], word.lower()
+        while currWord != "*":
+            currWord, match = engDict.readline().rstrip(), True
+            if len(currWord) == len(word):
+                score = 0
+                for i in range(len(word)):
+                    if word[i] == currWord[i]: score += 1
+                if score > bestScore[1]: bestScore = [currWord, score]
+        return bestScore[0]
+
+def SCInput(prompt=">>> "): #Author Harry Wilkins
+    for word in input(prompt).split(): return ''.join(AutoComplete.SpellCheck(word[:-1] if "," == word[-1] or "." == word[-1] else word))
+    #return " ".join([str(AutoComplete.SpellCheck(word)) for word in input(prompt).split(" ")])
