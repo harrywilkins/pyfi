b60e1b4bb8c271296f06f8107a012cd6a700692f
aidanhorton <aidan.horton@virginmedia.com>
2017-03-09 13:12:26 +0000

    Updated file versions
    Updated AdvancedInput.py to include comments
    Added __init__.py
    Removed unnecessary classes

--- pyfi/Debug.py
+++ pyfi/Debug.py
@@ -1,4 +1,4 @@
-class Debug(): #Author Harry Wilkins
+class Debug(): 
     supression = [0,0,0]
     log = {0:[],1:[],2:[]}
 
@@ -7,27 +7,10 @@ class Debug(): #Author Harry Wilkins
     #1       #Warning
     #2       #Error
     
-    def Log(_logType, _value):
+    def Log(self, _logType, _value): #Author Harry Wilkins
         Debug.log[_logType].append({0:"Log: ",1:"Warning: ",2:"Error: "}[_logType] + str(_value))
         if Debug.supression[_logType] == 0: print(Debug.log[_logType][-1])
         
-    def PrintLog():
+    def PrintLog(self): #Author Harry Wilkins
         for key in list(Debug.log.keys()):
             [print(x) for x in Debug.log[key]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
